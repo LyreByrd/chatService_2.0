d520323a91c0f142e7f4f5ae9711096c872e20c8
Guillermo Maycotte <gmaycotte93@gmail.com>
2018-12-05 11:46:22 -0600

    made quick branch for dev

--- server/server.js
+++ server/server.js
@@ -1,12 +1,6 @@
 require('dotenv').config()
-const fs = require('fs');
-const privateKey = fs.readFileSync('../../../etc/letsencrypt/live/gamaycotte.com/privkey.pem').toString();
-const certificate = fs.readFileSync('../../../etc/letsencrypt/live/gamaycotte.com/fullchain.pem').toString();
 const app = require('express')();
-const server = require('https').Server({
-  key: privateKey,
-  cert: certificate
-},app);
+const server = require('https').Server(app);
 const io = require('socket.io')(server);
 
 const dev = process.env.NODE_ENV !== 'production';
